c07fdb88a3e628cd470d8590c21a0cf157152296
Khum Regmi <kpr.regmi088@gmail.com>
2021-10-17 18:38:12 -0400

    made dummy data

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -22,7 +22,8 @@ class ShoppingList extends Component {
       <Container>
         <ListGroup>
           <TransitionGroup className="shopping-list">
-            {items.map(({ _id, name }) => (
+            <h1>What is this?</h1>
+            {/* {items.map(({ _id, name }) => (
               <CSSTransition key={_id} timeout={500} classNames="fade">
                 <ListGroupItem>
                   <Button
@@ -36,7 +37,7 @@ class ShoppingList extends Component {
                   {name}
                 </ListGroupItem>
               </CSSTransition>
-            ))}
+            ))} */}
           </TransitionGroup>
         </ListGroup>
       </Container>
